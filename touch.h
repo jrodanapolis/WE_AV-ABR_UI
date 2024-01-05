8083dd01017f5c232953ba25b2a0c647751ffa7d
jrodanapolis <142069286+jrodanapolis@users.noreply.github.com>
2024-01-04 17:42:07 -0700

    Update touch.h

--- touch.h
+++ touch.h
@@ -1,8 +1,6 @@
 /*******************************************************************************
- * Touch libraries:
- * FT6X36: https://github.com/strange-v/FT6X36.git
+ * Touch library:
  * GT911: https://github.com/TAMCTec/gt911-arduino.git
- * XPT2046: https://github.com/PaulStoffregen/XPT2046_Touchscreen.git
  ******************************************************************************/
 
 /********************************************
